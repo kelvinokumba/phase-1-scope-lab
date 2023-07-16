a3fecbe20b54f0f45733938d931bcdee2bdc7dc5
Kelvin okumba <okumbakelvin@gmail.com>
2023-07-16 16:41:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+var customerName = "bob"
+function fixTheScope() {
+    return customerName;
+}
+console.log(fixTheScope());
+ 
+function upperCaseCustomerName() {
+   let customerName = "bob";
+   customerName = customerName.toUpperCase();
+   return customerName;
+}
+console.log(upperCaseCustomerName());
+
+bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = "John Doe";
+  leastFavoriteCustomer = "Gilad"; // This line will cause an error
+  return leastFavoriteCustomer;
+}
+
+console.log (changeLeastFavoriteCustomer());
